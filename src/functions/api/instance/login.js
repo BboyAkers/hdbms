9d438ade84040a729018ed2e0bd9cd852afaf194
Austin Akers <austin@harperdb.io>
2024-12-19 11:32:16 -0800

    initial commit

--- src/functions/api/instance/login.js
+++
@@ -1,9 +0,0 @@
-import queryInstance from '../queryInstance';
-
-export default async ({ auth, url }) =>
-	queryInstance({
-		operation: { operation: 'login', username: auth.user, password: auth.pass },
-		auth,
-		url,
-		timeout: 5000,
-	});
